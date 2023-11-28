c92170519a742a3c186479291f69b295a75e239f
Anna D <hello.soandon@gmail.com>
2023-11-28 10:11:07 +0100

    Changing layout and adding animations

--- server.js
+++ server.js
@@ -16,16 +16,17 @@ app.get('/', (req, res)=>{
 
 app.get('/weather', (req, res)=>{
     console.log(req.query.cityName)
+    const queryCity = req.query.cityName.trim()
     axios({
         method: "get",
         url: `https://api.openweathermap.org/data/2.5/weather`,
         params: {
-            q:req.query.cityName,
+            q:queryCity,
             units:'metric',
             lang:'pl',
             appid:api_key,     
         }
-    }).then((response)=> res.json(response.data))
+    }).then((response)=> res.json(response.data)).catch((e)=>{ throw new Error(e.response.data.message)})
 })
 
 app.listen(PORT,()=>console.log(`Server running on ${PORT}`))
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import cloudy from './assets/cloud.png'
-import snow from './assets/snow.png'
-import thunder from './assets/thunder.png'
-import rain from './assets/rain.png'
-import sunny from './assets/sunny.png'
-import fog from './assets/fog.png'
+import cloudy from './assets/cloud@2x.png'
+import snow from './assets/snow@2x.png'
+import thunder from './assets/thunder@2x.png'
+import rain from './assets/rain@2x.png'
+import sunny from './assets/sunny@2x.png'
+import fog from './assets/fog@2x.png'
+import drop from './assets/rain_drop.png'
+import pressure_icon from './assets/pressure_icon@2x.png'
+import rabbit from './assets/winter_rabbit@2x.png'
 import { useMediaQuery } from 'react-responsive';
 import { BsMoonStars } from "react-icons/bs";
 import { MdOutlineWbSunny } from "react-icons/md";
@@ -16,16 +19,22 @@ import { WiHumidity } from "react-icons/wi";
 
 function App() {
   const [weather, setWeather]=useState<any>(null)
-  const [cityName, setCityName]=useState<FormDataEntryValue>('London')
+  const [error, setError] = useState('')
+  const [cityName, setCityName]=useState<FormDataEntryValue>('Zakopane')
+  const windowSize = useRef([window.innerWidth, window.innerHeight])
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-width: 1224px)'
   })
+
   useEffect(()=>{
     axios({
       method: "get",
       url: 'http://localhost:8080/weather',
       params: { cityName:cityName}
-    }).then(response=>setWeather(response.data)).catch(()=>console.log("something went wrong"))
+    }).then(response=>setWeather(response.data)).catch((e)=>{
+      setError('Error')
+      console.log(e)
+    })
   },[cityName])
   const submitHandler =(event: React.FormEvent<HTMLFormElement>)=>{
     event.preventDefault();
@@ -37,6 +46,9 @@ function App() {
       }
   }
  
+if(error) {
+  return <p>{error}</p>
+}
 if(!weather) {
   return <p>Sorry. We can't display weather. Try again later.</p>
 }
@@ -52,19 +64,24 @@ const sunsetTime = getTime(sunset)
 const sunriseTime = getTime(sunrise)
 const nowHour = new Date().getHours()
 const NightOrDay = nowHour  >= +sunsetTime.substring(0,2) ? 'night':'day'
-
 const info_boxes = [
   {
     name: 'wschód',
     value: <p>{sunriseTime}</p>,
-    icon: <MdOutlineWbSunny color="white" fontSize={16}/>
+    icon: <MdOutlineWbSunny color="grey" fontSize={16}/>
   },
   {
     name: 'zachód',
     value: <p>{sunsetTime}</p>,
-    icon: <BsMoonStars color="white" fontSize={16}/>
-  }
+    icon: <BsMoonStars color="grey" fontSize={16}/>
+  },
+  {
+    name: 'ciśnienie',
+    value: <div><img src={pressure_icon} className="pressure_icon" alt=""/><p>{pressure}hPa</p></div>,
+    icon: ''
+  },
 ]
+
 const weather_options={
   'Thunderstorm': thunder,
   'Drizzle': rain,
@@ -82,16 +99,62 @@ const weather_options={
   'Clear': sunny,
   'Clouds': cloudy,
 }
+console.log(windowSize.current[1])
+const anim = () => {
+  const windowWd = windowSize.current[0];
+  const windowHg = windowSize.current[1]
+  const quantity = isDesktopOrLaptop ? 20 : 6;
 
+  const getAnimationObject = () => {
+    let obj_styles = {
+      img: '',
+      name: '',
+      timing: 0,
+      top:'',
+      right:'',
+    }
+    switch(weather.weather[0].main) {
+      case 'Snow':
+        obj_styles = {...obj_styles, img:snow, name:'snow_falling', timing: 4.5}
+        break;
+      case 'Rain':
+        obj_styles = {...obj_styles, img:drop, name:'raining', timing: 3}
+        break;
+      default:
+        obj_styles = {...obj_styles, img:cloudy, name:'clouding', timing: 18};
+        break;
+    }
+    return obj_styles;
+  }
+ return Array.from({length: quantity}, (_,i)=>i).map((i)=>{
+  const randomNum = (description==='Snow'|| description==='Rain') ? Math.floor(Math.random() * windowWd) : Math.floor(Math.random() * windowHg)
+  const delay=i/2 + Math.floor(Math.random()* 3)
+  const animation_object = getAnimationObject()
+  return <img className="animation_obj" src={animation_object.img} 
+  style={{
+    top: (description==='Snow'|| description==='Rain') ? '-20px': randomNum + 'px', 
+    right: (description==='Snow'||description==='Rain') ? 0 + randomNum + 'px' :  '0px' , 
+    width: description==='Clouds' ? '110px':'',
+    height: description==='Clouds' ? 'auto':'',
+    animationDelay:`${delay}s`, 
+    animationName: animation_object.name, 
+    animationDuration: `${animation_object.timing}s`}} alt=''/>
+})
+}
+console.log(description)
   return (
     <div className={`App ${NightOrDay}`}>
+      {isDesktopOrLaptop &&<div className="animation">
+        {anim()}
+      </div>}
       {isDesktopOrLaptop && <form onSubmit={submitHandler}>
-        <input className="search_input" name="city" type="text" placeholder="Provide city"  />
+        <input className="search_input" name="city" type="text" placeholder="Wprowadź nazwę miasta"  />
         <button className="search_btn"><IoSearch color="white" fontSize={16}/></button>
         </form>}
       {
         weather !== null && weather !== undefined ? (    
           <div className={`weather_results ${isDesktopOrLaptop ? 'desktop': null} ${NightOrDay}`}>
+              {!isDesktopOrLaptop && anim()}
             <div className="weather_results--header">
               <div className="weather_results--header center">
               <p className="city_name">{weather.name}</p>
@@ -101,13 +164,19 @@ const weather_options={
 
               </div>
               <div className="weather_results--header bottom">
-                <div>
-                <WiHumidity color="white" fontSize={24}/>
-                  <p>wilgotność</p>
+                <img className="winter_rabbit" src={rabbit} alt="winter rabbit"/>
+                <div className="weather_results--header_box">
+                  <div>
+                    <WiHumidity color="white" fontSize={16}/>
+                    <p>wilgotność</p>
+                  </div>
                   <p>{humidity}%</p>
                 </div>
-                <div>
+                <div className="weather_results--header_box">
+                  <div>
                   <FaWind color="white" fontSize={16}/><p>wiatr</p>
+
+                  </div>
                   <p>{weather.wind.speed}m/sec</p>
                 </div>
               </div>
@@ -116,9 +185,11 @@ const weather_options={
               {info_boxes.map((box)=>{
                 return (
                   <div key={box.name} className="box">
+                  <div>
                     {box.icon}
-                  <p>{box.name}</p>
-                  {box.value}
+                    <p className="box-name">{box.name}</p>
+                  </div>
+                  <p className="box-value">{box.value}</p>
                 </div>
                 )
               })}
